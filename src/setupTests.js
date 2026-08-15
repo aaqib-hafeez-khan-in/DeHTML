fa97a3707621d8abfcdfd2e289c9d215df815a15
Aaqibhafeez Khan <githubaaqibhafeezkhan@gmail.com>
2026-08-15 13:31:54 +0530

    test: polyfill innerText for jsdom

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom'
+
+Object.defineProperty(HTMLElement.prototype, 'innerText', {
+  configurable: true,
+  get() {
+    return this.textContent || ''
+  },
+  set(value) {
+    this.textContent = value
+  },
+})
